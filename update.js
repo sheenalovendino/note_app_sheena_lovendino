4d74a90b525771edebef05f92b8a784549efac84
Sheena <slovendino@sorsu.edu.ph>
2022-10-24 09:00:46 +0800

    edited update.js

--- update.js
+++ update.js
@@ -1,16 +1,17 @@
-const fs = require('fs')
-const { isModuleNamespaceObject } = require('util/types')
+const fs = require ('fs')
 
-const update = function(note, oldNote){
-    const updateNote = JSON.parse(oldNote)
+const update = function(note, oldNote) {
+    const newNote = JSON.parse(oldNote).map(function(n,i){
 
-    const finalNote = updateNote.map(function(n,id) {
-        if (n.id = note.id) {
+        if (n.id == note.id){
             n.title = note.title
             n.body = note.body
-            return n
+
         }
+        return n
     })
-    fs.writeFileSync('note.txt', JSON.stringify(finalNote))
+
+    fs.writeFileSync('note.txt', JSON.stringify(newNote))
 }
+
 module.exports = update
